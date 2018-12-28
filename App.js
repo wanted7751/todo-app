341a55836159ab7156c2993fee9c19bacfe5ff40
parkminho <minho4377@daum.net>
2018-12-29 01:28:23 +0900

    #9 Saving To Dos part One

--- App.js
+++ App.js
@@ -2,42 +2,81 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Dimensions, TextInput, Platform } from "react-native";
 import { ScrollView } from 'react-native-gesture-handler';
 import ToDo from "./ToDo";
+import {AppLoading} from "expo";
+import uuidv1 from "uuid/v1";
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newToDo:""
-  }
+    newToDo: "",
+    loadedToDos: false
+  };
+  componentDidMount = () => {
+    this._loadToDos();
+  };
   render() {
-    const { newToDo} = this.state;
+    const { newToDo, loadedToDos } = this.state;
+    if (!loadedToDos) {
+      return <AppLoading />;
+    }
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content"/>
-        <Text style={styles.title}>kawai to do </Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-        <TextInput 
-          style={styles.input} 
-          placeholder={"NEW TO DO "} 
-          value={newToDo} 
-          onChangeText={this._controllNewTodo}
-          placeholderTextColor={"#999"} 
-          returnKeyType={"done"}
-          autoCorrect={false}
-          autoCapitalize={"none"}
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+            onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
-            
+            <ToDo text={"Hello I'm a To Do"} />
           </ScrollView>
-
         </View>
       </View>
     );
   }
-  _controllNewTodo = text => {
+  _crontollNewToDo = text => {
     this.setState({
       newToDo: text
-    })
+    });
+  };
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    });
+  };
+  _addToDo = () =>{
+    const{newToDo} = this.state;
+    if(newToDo !==""){
+      this.setState(prevState =>{
+        const ID = uuidv1();  
+        const newToDoObject = {
+          [ID] : {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createAt: Date.now()
+            
+          }
+        };
+        const newState = {
+          ...prevState,
+          newToDo:"",
+          toDos:{
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        }
+        return{...newState};
+      })
+    }
   }
 }
 
@@ -51,18 +90,18 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 30,
     marginTop: 50,
-    fontWeight: "900",
-    marginBottom: 50
+    fontWeight: "200",
+    marginBottom: 30
   },
   card: {
     backgroundColor: "white",
     flex: 1,
-    width: width - 50,
+    width: width - 25,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     ...Platform.select({
-      ios:{
-        shadowColor:"rgb(50, 50, 50)",
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
         shadowOffset: {
@@ -70,24 +109,18 @@ const styles = StyleSheet.create({
           width: 0
         }
       },
-      android:{
+      android: {
         elevation: 3
       }
-
-    }),
-
+    })
   },
-  input:{
-    padding:20,
-    borderBottomColor:"#bbb",
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
     borderBottomWidth: 1,
-    fontSize: 20,  
-    
+    fontSize: 25
   },
-  
-  toDos:{
-    alignItems:"center"
+  toDos: {
+    alignItems: "center"
   }
-
-
 });
